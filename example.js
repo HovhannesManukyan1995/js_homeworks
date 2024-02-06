1ee5ea545a578a29dd685de4a3490a894e391a90
HovhannesManukyan1995 <hovom0632@gmail.com>
2024-02-06 22:19:00 +0400

    just a commit

---
+++ example.js
@@ -0,0 +1,56 @@
+const prompt = require("prompt-sync")();
+//let sa=prompt('tell me why: ')
+//let hi='hello world';
+//console.log(sa,hi)
+
+//let arr1=[NaN, 0, 15, false, -22, '', undefined, 47, null];
+//let arr2=[];
+//for(const i of arr1){
+//if(i){arr2.push(i)}
+//}
+//console.log(arr2)
+
+
+
+//let arr1=[NaN, 0, 15, false, -22, '', undefined, 47, null];
+//var item = arr1[Math.floor(Math.random()*arr1.length)];
+//console.log(item)
+
+//let arr1=[NaN, 0, 15, false, -22, '', undefined, 47, null];
+//console.log(arr1)
+//let sp=arr1.splice(2,1)
+//arr1.splice(0,0,sp[0])
+//console.log(arr1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
